7f011df6c2de3fd3e74d67e386de1f539579d62e
jaxono <61395595+jaxono@users.noreply.github.com>
2020-07-30 09:08:46 +1200

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 # Declare Variables
 from math import inf
 
-UNIT_NAMES = ["g", "mg", "kg", "ml", "l", "t", "oz", "lbs", "st", "ton", "c", "tbsp", "dstspn", "tsp"]
-UNIT_SIZES = [1, 0.001, 1000, 1, 1000, 1000000, 28.3495, 453.592, 6350.29, 907185, 128, 15, 10, 5]
+UNIT_NAMES = ["g", "mg", "kg", "ml", "l", "t", "oz", "lbs", "st", "ton", "c", "tbsp", "dstspn", "tsp", "pinch"]
+UNIT_SIZES = [1, 0.001, 1000, 1, 1000, 1000000, 28.3495, 453.592, 6350.29, 907185, 128, 15, 10, 5, 0.355625]
+
+USELESS_WORDS = ["the", "of", "a"]
 
 recipe_name = input("Recipe Name: ").title()  # Get Name
+
+origin = input("Place of Origin: ").title()  # Get Location
 
 ingrs = []
 
 
 class Ingr:
-    def __init__(self, name, size):
+    def __init__(self, name, size, united):
         self.name = name
         self.size = size
+        self.united = united
 
 
 ingr_name = ""
@@ -35,7 +40,7 @@ while True:
         print('"Original Serving Size" cannot be 0, please re-enter.')
         continue
 
-    # Warn if ratio is to big/small/ or force re-entry infinite/0/negative
+    # Warn if ratio is to big/small or force re-entry infinite/0/negative
 
     if ratio <= 0:
         print("That ratio is 0 or less, please re-enter.")
@@ -86,18 +91,68 @@ while True:
 
     # Check formatting
 
-    if ingredient.count(" ") < 1:
-        print("Syntax Error")
-        continue
+    ingr_size = "1"
+    unit = ""
+    ingr_name = ""
+
+    while True:
+        continue_now = False
+        try:
+            front_word = ingredient[:ingredient.index(" ")]
+        except:
+            break
+
+        for useless_word in USELESS_WORDS:
+            if front_word.strip().lower() == useless_word:
+                ingredient = ingredient[ingredient.index(" ") + 1:]
+                continue_now = True
+        if continue_now:
+            continue
+
+        try:
+            if ingr_size.count("/") == 0:
+                float(front_word.strip().lower())
+                ingr_size = front_word.strip().lower()
+            elif ingr_size.count("/") == 1:
+                ingr_size = front_word.strip().lower()
+            try:
+                    ingredient = ingredient[ingredient.index(" ") + 1:]
+            except:
+                break
+            continue
+        except:
+            pass
+
+        for unit_name in UNIT_NAMES:
+            if front_word.strip().lower() == unit_name:
+                unit = front_word.strip().lower()
+                try:
+                    ingredient = ingredient[ingredient.index(" ") + 1:]
+                except:
+                    break
+                continue_now = True
+        if continue_now:
+            continue
+
+        break
+
+    ingr_name = ingredient.title()
+
+    ingr_united = not unit == ""
+
+    #print()
+    #print(ingr_size)
+    #print(unit)
+    #print(ingr_name)
 
     try:
 
         # Separate data
 
-        ingr_size = ingredient[:ingredient.index(" ")]
-        ingredient = ingredient[ingredient.index(" ") + 1:]
-        unit = ingredient[:ingredient.index(" ")].lower()
-        ingr_name = ingredient[ingredient.index(" ") + 1:].title()
+        #ingr_size = ingredient[:ingredient.index(" ")]
+        #ingredient = ingredient[ingredient.index(" ") + 1:]
+        #unit = ingredient[:ingredient.index(" ")].lower()
+        #ingr_name = ingredient[ingredient.index(" ") + 1:].title()
 
         # Process ingr_size
 
@@ -119,13 +174,17 @@ while True:
 
         # Convert to the gram
 
-        ingr_size = ingr_size * UNIT_SIZES[UNIT_NAMES.index(unit)]
+        if ingr_united:
+            ingr_size = ingr_size * UNIT_SIZES[UNIT_NAMES.index(unit)]
 
         # Add the ingredient to the list
 
-        ingrs.append(Ingr(ingr_name, ingr_size))
+        ingrs.append(Ingr(ingr_name, ingr_size, ingr_united))
 
-        print("Added " + str(round(ingr_size, 3)).rstrip("0").rstrip(".") + " g " + ingr_name)
+        if ingr_united:
+            print("Added " + str(round(ingr_size, 3)).rstrip("0").rstrip(".") + " g of " + ingr_name)
+        else:
+            print("Added " + str(round(ingr_size, 3)).rstrip("0").rstrip(".") + " " + ingr_name)
 
     except ValueError:
         print("Syntax Error, please re-enter")
@@ -136,7 +195,8 @@ while True:
 # Print out new recipe
 
 print()
-print("Modern Recipe:\n\n{}\n".format(recipe_name))
+print("Modern Recipe:\n\n{}".format(recipe_name))
+print("From " + origin + "\n")
 
 # Loop though all ingredients
 
@@ -150,15 +210,19 @@ while i < len(ingrs):
 
     # Convert to mg or kg if appropriate
 
-    if ingr_size < 1:
-        ingr_size = ingr_size * 1000
-        unit = "mg"
+    if ingrs[i].united:
+        if ingr_size < 1:
+            ingr_size = ingr_size * 1000
+            unit = "mg"
 
-    elif ingr_size >= 1000:
-        ingr_size = ingr_size / 1000
-        unit = "kg"
+        elif ingr_size >= 1000:
+            ingr_size = ingr_size / 1000
+            unit = "kg"
 
         # Print out ingredient
 
-    print(str(round(ingr_size, 3)).rstrip("0").rstrip(".") + " " + unit + " " + ingrs[i].name)
+    if ingrs[i].united:
+        print(str(round(ingr_size, 3)).rstrip("0").rstrip(".") + " " + unit + " of " + ingrs[i].name)
+    else:
+        print(str(round(ingr_size, 3)).rstrip("0").rstrip(".") + " " + ingrs[i].name)
     i += 1
